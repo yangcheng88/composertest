7909e2861e9900621ed59313c1ee2818aa846183
yangcheng <yangcheng@diandong.com>
2021-07-20 13:43:30 +0800

    test

---
+++ src/SayHello.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Yangcheng88\Composertest;
+
+class SayHello
+{
+    private $name;
+
+    public function __construct($name='yangcheng')
+    {
+        $this->name = $name;
+    }
+
+    public function run()
+    {
+        echo $this->name . ' Hello !';
+    }
+}
